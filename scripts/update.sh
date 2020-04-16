90a3ac5366c7738e2349e741dcc4573fdaa144a8
robovoid123 <headtotail12345@gmail.com>
2020-04-16 16:35:29 +0545

    nvim config changes and random stuffs

--- scripts/update.sh
+++ scripts/update.sh
@@ -21,18 +21,18 @@ function sync_in {
 function sync_out {
     #function_body
     echo "Syncing to device"
-    rsync -ra  /home/robovoid/dot-files/qtile  /home/robovoid/.config/qtile &
-    rsync -ra  /home/robovoid/dot-files/nvim  /home/robovoid/.config/nvim &
-    rsync -ra  /home/robovoid/dot-files/alacritty  /home/robovoid/.config/alacritty &
-    rsync -ra  /home/robovoid/dot-files/ranger  /home/robovoid/.config/ranger &
+    rsync -ra  /home/robovoid/dot-files/qtile/  /home/robovoid/.config/qtile &
+    rsync -ra  /home/robovoid/dot-files/nvim/  /home/robovoid/.config/nvim &
+    rsync -ra  /home/robovoid/dot-files/alacritty/  /home/robovoid/.config/alacritty &
+    rsync -ra  /home/robovoid/dot-files/ranger/  /home/robovoid/.config/ranger &
     rsync -ra  /home/robovoid/dot-files/.zshrc  /home/robovoid/.zshrc &
-    rsync -ra  /home/robovoid/dot-files/conf  /home/robovoid/.config/picom.conf &
-    rsync -ra  /home/robovoid/dot-files/dunst  /home/robovoid/.config/dunst &
-    rsync -ra  /home/robovoid/dot-files/scripts  /home/robovoid/scripts &
-    rsync -ra  /home/robovoid/dot-files/bspwm  /home/robovoid/.config/bspwm &
-    rsync -ra  /home/robovoid/dot-files/polybar  /home/robovoid/.config/polybar &
-    rsync -ra  /home/robovoid/dot-files/sxhkd  /home/robovoid/.config/sxhkd &
-    rsync -ra  /home/robovoid/dot-files/vifm  /home/robovoid/.config/vifm &
+    rsync -ra  /home/robovoid/dot-files/picom.conf  /home/robovoid/.config/picom.conf &
+    rsync -ra  /home/robovoid/dot-files/dunst/  /home/robovoid/.config/dunst &
+    rsync -ra  /home/robovoid/dot-files/scripts/  /home/robovoid/scripts &
+    rsync -ra  /home/robovoid/dot-files/bspwm/  /home/robovoid/.config/bspwm &
+    rsync -ra  /home/robovoid/dot-files/polybar/  /home/robovoid/.config/polybar &
+    rsync -ra  /home/robovoid/dot-files/sxhkd/  /home/robovoid/.config/sxhkd &
+    rsync -ra  /home/robovoid/dot-files/vifm/  /home/robovoid/.config/vifm &
 }
 
 case "$1" in
