a9cebc5781413ac83c983e54b58ff4bf6c4a085f
robovoid <headtotail12345@gmail.com>
2020-10-30 10:00:13 +0545

    update

--- .config/scripts/dmenu_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dmenu_run  -i -fn 'JetBrains Mono Nerd Font:size=10' -nb '#1d2021' -nf '#b8bb26' -sb '#689d6a' -sf '#1d2021' -p "dmenu:"
--- .config/scripts/shutdown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $(echo -e 'Yes\nNo' | ~/scripts/dmenu.sh "Do you want to shutdown: ") == 'Yes' ]
-then
-    systemctl poweroff
-fi
-
